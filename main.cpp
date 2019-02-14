baee15369002ae3e550fc5f9c1969c594166648f
PlsDeleteMyLife <cdranton@gmail.com>
2019-02-14 17:33:33 +0700

    Fixes of function of memory and using less space.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include <cmath>
 #include <string>
 
-int sc_memoryInit(int massive[]);
+int massive[100];
+
+int sc_memoryInit();
 
 int sc_memorySet(int adress, int value);
 
@@ -17,59 +19,65 @@ int sc_memoryLoad(char *filename);
 int main(void)
 {
 	system("clear");
-	/* inicializing */
-	int massive[100];
 	int adress,value;
-	sc_memoryInit(massive);
-	/* inputing shit */
+	sc_memoryInit();
 	std::cout << "Input ur adress: ";
 	std::cin >> adress;
-	std::cout << std::endl << "Input ur value: ";
-	std::cin >> value;
-	/* 		 */
-	/* memory set */
-	bool trying = false;
-	while(!trying)
+	std::cout << std::endl;
+	int track_set = sc_memorySet(adress, value); // = value
+	int track_get = sc_memoryGet(adress, &value); // = *value
+	if(track_set != -1)
 	{
-		system("clear");
-		int error_track = sc_memorySet(massive[adress], value);
-		if(error_track == -1)
-		{
-			std::cout << "You input a wrong 'adress' value" << std::endl << "You need new try? [y/n]";
-			char try_new_adress;
-			std::cin >> try_new_adress; std::cout << std::endl;
-			if(try_new_adress = 'y')
-			{
-				trying = false;
-			}
-			else if(try_new_adress = 'n')
-			{
-				trying = true;
-			}
-		}
+		std::cout << "Array[" << adress << "] = " << track_set << "\n";
+		std::cout << "Array[" << adress << "] has cell of memory: " << track_get << "\n";
 	}
 	system("pause");
 	return 0;
 }
 
-int sc_memoryInit(int array[])
+int sc_memoryInit()
 {
 	for(auto i = 0; i < 100; i++)
-		array[i] = 0;
+		massive[i] = 0;
 }
 
 int sc_memorySet(int adress, int value)
 {
-	bool flag_of_work = true;
-	if(adress)
+	if(adress > 99 || adress < 0)
 	{
-		if(adress > 100 || adress < 0)
-			return -1;
-		else
-		{
-			/* invoiding table symbols */
-			//value = ?;
-		}
+		std::cout << "Wrong number of memeory!(sc_memorySet)\n";
+		return -1;
+	}
+	else
+	{
+		std::cout << "Input the value of massive[" << adress << "]: ";
+		std::cin >> value; std::cout << std::endl;
+		massive[adress] = value;
 	}
 	return value;
+}
+
+int sc_memoryGet(int adress, int *value)
+{
+	int index;
+	if(adress > 99 || adress < 0)
+	{
+		std::cout << "Wrong number of memeory!(sc_memoryGet)\n";
+		return -1;
+	}
+	else
+	{
+		index = *value;
+		return index;
+	}
+}
+
+int sc_memorySave(char *filename)
+{
+	//
+}
+
+int sc_memoryLoad(char *filename)
+{
+	//
 }
